da7520a713f2d0f6cda28906a0f5cecf9d0e7bb8
Andrew Montenigro <andrew.montenigro@gmail.com>
2015-04-20 23:20:10 -0500

    Added parser??

--- src/express/Express.java
+++ src/express/Express.java
@@ -4,27 +4,40 @@
  *Function: 
  * 
  */
-
 package express;
+
+import express.Token.TokenType;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import java.util.SortedSet;
+//import java.util.regex.Pattern;
+//import java.util.regex.Matcher;
 
-public class Express{		
-	public static void main(String[] args){
-		//variable(s)
-		Scanner in = new Scanner(System.in);
-		String input = "";
+public class Express {
 
-		input = in.nextLine();
-		//output
-		System.out.println("The expression '"+ input +"':");
-		System.out.println("Token Type\t\tLexeme\t\tValue");
-		System.out.println("=============================================");
+    public static void main(String[] args) {
+        //variable(s)
+        Scanner in = new Scanner(System.in);
+        String input = "";
+        input = in.nextLine();
+        //output
+        System.out.println("The expression '" + input + "':");
+        System.out.println("Token Type\t\tLexeme\t\tValue");
+        System.out.println("=============================================");
 
-		ArrayList<Token> tokens = Lexer.lex(input.replaceAll(" ", ""));
-		for (Token token : tokens)
-			System.out.println(token);
-	}
+        ArrayList<Token> tokens = Lexer.lex(input);
+        for (Token token : tokens) {
+            //if(token.getType() == TokenType.T_WHITESPACE) continue;
+            if (token.getType().equals(TokenType.T_WHITESPACE)) {
+                continue;
+            }
+            System.out.println(token);
+        }
+        System.out.printf("%n%nTesting parse output%n");
+
+        SortedSet<Token> parseTree = Parser.parse(tokens);
+        for (Token t : parseTree) {
+            System.out.println(t);
+        }
+    }
 }
--- src/express/Token.java
+++ src/express/Token.java
@@ -1,53 +1,73 @@
 package express;
 
-public class Token{
-	// Token types
-	public static enum TokenType{T_ID, T_DOUBLE, T_PLUS, T_MINUS, T_MULT, T_DIV, T_EQUALS,
-		T_LEFT_PAREN, T_RIGHT_PAREN}
+import java.util.Comparator;
 
-	// Variables
-	public String lexedItem;
-	public double value;
-	public TokenType tokType;
+public class Token {
 
-	// Overriding the toString functionc
-	@Override
-	public String toString(){
-		return String.format("%-20s\t%4s\t\t%.3f", this.tokType, this.lexedItem, this.value);
-	}
+    // Token types
 
-	public Token(String lexerInput, TokenType t){
-		this.lexedItem = lexerInput;
-		this.tokType = t;
+    public static enum TokenType {
 
-		if(tokType.equals(TokenType.T_ID))
-			value = 0.0;
-		if(lexedItem.substring(0,1).matches("[0-9]")){
-			this.tokType = TokenType.T_DOUBLE;
-			value = Double.parseDouble(lexedItem);
-		}
+        T_WHITESPACE(0),
+        T_ID(1),
+        T_DOUBLE(1),
+        T_PLUS(2),
+        T_MINUS(2),
+        T_MULT(3),
+        T_DIV(3),
+        T_LEFT_PAREN(4),
+        T_RIGHT_PAREN(4),
+        T_EQUALS(5);
 
-	}
+        private final Integer priority;
 
-	public Token(String lexerInput){
-		this.lexedItem = lexerInput;
-		if(lexedItem.equals("("))
-			tokType = TokenType.T_LEFT_PAREN;
-		else if(lexedItem.equals(")"))
-			tokType = TokenType.T_RIGHT_PAREN;
-		else if(lexedItem.equals("="))
-			tokType = TokenType.T_EQUALS;
-		else if(lexedItem.equals("-?[0-9]+"))
-			tokType = TokenType.T_DOUBLE;
-		else if(lexedItem.equals("+"))
-			tokType = TokenType.T_PLUS;
-		else if(lexedItem.equals("-"))
-			tokType = TokenType.T_MINUS;
-		else if(lexedItem.equals("*"))
-			tokType = TokenType.T_MULT;
-		else if(lexedItem.equals("/"))
-			tokType = TokenType.T_DIV;
-		else if(lexedItem.equals("^[^\\d\\W]\\w*\\Z"))
-			tokType = TokenType.T_ID;
-	}
+        TokenType(int priority) {
+            this.priority = priority;
+        }
+
+        public int isHigherThan(TokenType other) {
+            return this.priority - other.priority;
+        }
+    }
+
+    // Variables
+    public String lexedItem;
+    public double value;
+    public TokenType tokType;
+
+    // Overriding the toString functionc
+    @Override
+    public String toString() {
+        return String.format("%-20s\t%4s\t\t%.3f", this.tokType, this.lexedItem, this.value);
+    }
+
+    public String getText() {
+        return this.lexedItem;
+    }
+
+    public TokenType getType() {
+        return this.tokType;
+    }
+
+    public double getValue() {
+        return this.value;
+    }
+
+    public Token(Double value) {
+        this.lexedItem = value.toString();
+        this.tokType = TokenType.T_DOUBLE;
+        this.value = value;
+    }
+
+    public Token(String lexerInput, TokenType t) {
+        this.lexedItem = lexerInput;
+        this.tokType = t;
+        this.value = 0.0;
+    }
+    public static Comparator<Token> tokenComparator = new Comparator<Token>() {
+        @Override
+        public int compare(Token t1, Token t2) {
+            return t1.getType().isHigherThan(t2.getType());
+        }
+    };
 }
