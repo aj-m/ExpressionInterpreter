628054e9929a5481ec095f7b85e4a2a631e073b2
Andrew Montenigro <andrew.montenigro@gmail.com>
2015-04-20 23:39:51 -0500

    Added file input

--- src/express/Express.java
+++ src/express/Express.java
@@ -7,9 +7,13 @@
 package express;
 
 import express.Token.TokenType;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.SortedSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 //import java.util.regex.Pattern;
 //import java.util.regex.Matcher;
 
@@ -17,7 +21,18 @@ public class Express {
 
     public static void main(String[] args) {
         //variable(s)
-        Scanner in = new Scanner(System.in);
+        Scanner in = null;
+        try {
+            if (args.length > 0) {
+                in = new Scanner(new FileReader(args[0]));
+            }
+        } catch (FileNotFoundException ex) {
+            in = new Scanner(System.in);
+        } finally {
+            if (in == null) {
+                in = new Scanner(System.in);
+            }
+        }
         String input = "";
         input = in.nextLine();
         //output
